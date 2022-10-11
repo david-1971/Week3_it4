c29de244b65e9dee92435aff5967d536e614797d
David <ladbl1971@gmail.com>
2022-10-11 18:42:36 +0400

    first visual studio push

---
+++ Week2 -it2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Week2__it2
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.ForegroundColor = ConsoleColor.Red;
+           
+
+            Console.Write("aa");
+            Console.Write("aa bb");
+            Console.WriteLine("dddd");
+            Console.WriteLine("uuuu");
+            Console.WriteLine("david");
+            Console.WriteLine("katsadze");
+
+            Console.WriteLine("  /|");
+            Console.WriteLine(" /_|");
+            Console.WriteLine("/__|");
+           
+           
+    }
+}
